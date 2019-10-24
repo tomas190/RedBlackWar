993dbd7290b29761ad387e0c1f84bccd87d386d0
tomas <tomas@gamstudio.net>
2019-10-24 18:11:26 +0800

    QA测试

--- game/internal/cards_rbdz.go
+++ game/internal/cards_rbdz.go
@@ -71,7 +71,7 @@ func (this *RBdzDealer) Deal() ([]byte, []byte) {
 //获取牌型并比牌
 func (r *Room) RBdzPk(a []byte, b []byte) {
 	a = []byte{14,13,24}  //A  豹子
-	b = []byte{62,28,9}  //Q  豹子
+	b = []byte{62,61,40}  //Q  豹子
 
 	// 14,46,62  |   12,28,44  //豹子
 	// 7,8,9     |   3,4,5     //顺金
