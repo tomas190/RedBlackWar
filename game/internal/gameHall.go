404118c7fff287e4a851b8a41e94215b96f0a575
tomas <tomas@gamstudio.net>
2020-01-07 11:39:08 +0800

    处理断线重连

--- game/internal/gameHall.go
+++ game/internal/gameHall.go
@@ -40,37 +40,6 @@ func (gh *GameHall) CreatGameRoom() *Room {
 
 //PlayerJoinRoom 玩家大厅加入房间
 func (gh *GameHall) PlayerJoinRoom(rid string, p *Player) {
-	for _, r := range gh.roomList {
-		for _, v := range r.PlayerList {
-			if v != nil && v.Id == p.Id {
-				p.room = r
-				msg := &pb_msg.JoinRoom_S2C{}
-				roomData := p.room.RspRoomData()
-				msg.RoomData = roomData
-				if p.room.GameStat == DownBet {
-					msg.GameTime = DownBetTime - p.room.counter
-				} else {
-					msg.GameTime = SettleTime - p.room.counter
-				}
-				p.SendMsg(msg)
-
-				//玩家各注池下注金额
-				pool := &pb_msg.PlayerPoolMoney_S2C{}
-				pool.DownBetMoney = new(pb_msg.DownBetMoney)
-				pool.DownBetMoney.RedDownBet = p.DownBetMoneys.RedDownBet
-				pool.DownBetMoney.BlackDownBet = p.DownBetMoneys.BlackDownBet
-				pool.DownBetMoney.LuckDownBet = p.DownBetMoneys.LuckDownBet
-				p.SendMsg(pool)
-
-				p.room.GetGodGableId()
-				//更新列表
-				p.room.UpdatePlayerList()
-				maintainList := p.room.PackageRoomPlayerList()
-				p.room.BroadCastMsg(maintainList)
-				return
-			}
-		}
-	}
 
 	for _, room := range gh.roomList {
 		if room != nil && room.RoomId == rid { // 这里要不要遍历房间，查看用户id是否存在
