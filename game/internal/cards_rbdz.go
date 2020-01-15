96226ff2e4257aeb5be79caee5538aae220679cb
tomas <tomas@gamstudio.net>
2020-01-15 09:34:05 +0800

    test

--- game/internal/cards_rbdz.go
+++ game/internal/cards_rbdz.go
@@ -205,12 +205,13 @@ func (r *Room) RBdzPk(a []byte, b []byte) {
 	res.BlackCard = r.Cards.BlackCard
 
 	surPool := FindSurplusPool()
-	AllHistoryWin = surPool.HistoryWin
-	AllHistoryLose = surPool.HistoryLose
-	sur.PlayerNum = surPool.PlayerNum
+	if surPool != nil {
+		AllHistoryWin = surPool.HistoryWin
+		AllHistoryLose = surPool.HistoryLose
+		sur.PlayerNum = surPool.PlayerNum
 
-	log.Debug("盈余池 %v", surPool)
-
+		log.Debug("盈余池 %v", surPool)
+	}
 	//获取Pot池Win
 	if ag.Weight > bg.Weight { //redWin
 		//log.Debug("Red Win ~")
