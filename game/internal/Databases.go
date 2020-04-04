d83cb922f8e2992c95e0b028fa408e4005ecd64b
tomas <tomas@gamstudio.net>
2020-04-04 14:42:16 +0800

    解决房间卡主

--- game/internal/Databases.go
+++ game/internal/Databases.go
@@ -141,8 +141,7 @@ func InsertSurplusPool(sur *SurplusPoolDB) {
 	s, c := connect(dbName, surPlusDB)
 	defer s.Close()
 
-	//sur.PoolMoney = (sur.HistoryLose - (sur.HistoryWin * 1)) * 0.5
-	sur.PoolMoney = 10000  // todo
+	sur.PoolMoney = (sur.HistoryLose - (sur.HistoryWin * 1)) * 0.5
 	SurplusPool = sur.PoolMoney
 	log.Debug("surplusPoolDB 数据: %v", sur.PoolMoney)
 
